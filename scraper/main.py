71035ace255f73daac211bc04d5868d20fb61311
sbshaun <sbshaun7@gmail.com>
2023-11-13 00:47:34 -0800

    refactored

--- scraper/main.py
+++ scraper/main.py
@@ -1,43 +1,58 @@
 import os
 from datetime import datetime
 from src.jobspy import scrape_jobs
-from s3.s3_helpers import upload_to_s3  # Make sure to import your helper function
+from s3.s3_helpers import upload_to_s3
+from src.processors.aggregator import aggregate_csv_files
 
-# Get the directory in which the current script is located
-current_dir = os.path.dirname(os.path.abspath(__file__))
+def setup_data_directory(base_path: str, sub_path: str) -> str:
+    data_dir = os.path.join(base_path, sub_path)
+    os.makedirs(data_dir, exist_ok=True)
+    return data_dir
 
-# Ensure the 'data' directory exists within the same directory as the script
-data_dir = os.path.join(current_dir, 'data')
-os.makedirs(data_dir, exist_ok=True)
+def run_scraper(site_names, search_term, location, results_wanted, country, output_path):
+    jobs = scrape_jobs(
+        site_name=site_names,
+        search_term=search_term,
+        location=location,
+        results_wanted=results_wanted,
+        country_indeed=country
+    )
+    jobs.to_csv(output_path, index=False)
+    print(f"Found {len(jobs)} jobs for '{search_term}', saved to {output_path}")
 
-jobs = scrape_jobs(
-    site_name=["indeed", "linkedin", "zip_recruiter", "glassdoor"],
-    search_term="Software Engineer",
-    location="Vancouver, BC",
-    results_wanted=30,
-    country_indeed='Canada'  # only needed for indeed / glassdoor
-)
-print(f"Found {len(jobs)} jobs")
+def upload_data_to_s3(bucket_name, s3_key, data_path):
+    if bucket_name:
+        upload_to_s3(bucket_name, s3_key, data_path)
+        print(f"Jobs uploaded to S3 bucket {bucket_name} at {s3_key}")
+    else:
+        print("S3 bucket name is not set in .env file.")
+        
+def main():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    individual_run_dir = setup_data_directory(current_dir, 'data/individual_run')
+    aggregated_dir = setup_data_directory(current_dir, 'data/aggregated')
+    
+    search_terms = ["Software Engineer Intern", "Software Engineer Co-op", "Software Developer Intern", "Software Developer Co-op"]  # Add more search terms as needed
+    site_names = ["indeed", "linkedin", "zip_recruiter", "glassdoor"]
+    location = "Vancouver, BC"
+    results_wanted = 10
+    country = 'Canada'
+    
+    for term in search_terms:
+        timestamp = datetime.now().strftime('%Y%m%dT%H%M%S')  # Compact timestamp
+        term_filename = term.replace(" ", "_").lower()
+        csv_filename = f'jobs_{term_filename}_{timestamp}.csv'
+        jobs_csv_path = os.path.join(individual_run_dir, csv_filename)
+        
+        run_scraper(site_names, term, location, results_wanted, country, jobs_csv_path)
+    
+    aggregated_file_path = aggregate_csv_files(individual_run_dir, aggregated_dir)
+    
+    date_prefix = datetime.now().strftime('%Y/%m/%d')  # S3 prefix in 'YYYY/MM/DD' format
+    s3_key_aggregated = f"{date_prefix}/aggregated_jobs.csv"
+    upload_data_to_s3(os.getenv('S3_BUCKET_NAME'), s3_key_aggregated, aggregated_file_path)
 
-# Generate a date prefix and timestamp for the filename
-date_prefix = datetime.now().strftime('%Y/%m/%d')  # S3 prefix in 'YYYY/MM/DD' format
-timestamp = datetime.now().strftime('%Y%m%dT%H%M%S')  # Compact timestamp
-
-# Construct the S3 key with the prefix and the timestamped filename
-s3_key = f"{date_prefix}/jobs_{timestamp}.csv"
-
-# Save the jobs to a CSV file in the 'data' directory with a timestamp
-jobs_csv_path = os.path.join(data_dir, f'jobs_{timestamp}.csv')
-jobs.to_csv(jobs_csv_path, index=False)
-
-print(f"Jobs saved to {jobs_csv_path}")
-
-# Now use the helper function to upload the file to S3
-s3_bucket_name = os.getenv('S3_BUCKET_NAME')
-if s3_bucket_name:
-    upload_to_s3(s3_bucket_name, s3_key, jobs_csv_path)
-    print(f"Jobs uploaded to S3 bucket {s3_bucket_name} at {s3_key}")
-else:
-    print("S3 bucket name is not set in .env file.")
-
-print("Done!")
+if __name__ == "__main__":
+    print("Starting the scraping process...")
+    main()
+    print("Scraping process completed.")
--- scraper/src/processors/aggregator.py
+++ scraper/src/processors/aggregator.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import os
 from glob import glob
-from datetime import datetime
 from typing import List
 
-def aggregate_csv_files(source_dir: str, output_dir: str) -> None:
+def aggregate_csv_files(source_dir: str, output_dir: str) -> str:
+    """
+    Aggregate all CSV files from the source directory into a single file in the output directory.
+    Duplicate entries are identified by the 'job_url' column and removed.
+    Returns the path of the aggregated CSV file
+    Optional: After successful aggregation, individual CSV files are deleted.
+    """
     # Convert relative paths to absolute paths
     source_dir_abs = os.path.abspath(source_dir)
     output_dir_abs = os.path.abspath(output_dir)
@@ -27,23 +32,47 @@ def aggregate_csv_files(source_dir: str, output_dir: str) -> None:
     # Generate a list of DataFrames by reading each CSV file
     df_list = [pd.read_csv(f, index_col=None, header=0) for f in all_files]
     
-    # Concatenate all DataFrames into one and drop duplicates based on the 'job_url' column
+    # Concatenate all DataFrames into one
     if df_list:
-        combined_df: pd.DataFrame = pd.concat(df_list, axis=0, ignore_index=True).drop_duplicates(subset='job_url')
+        combined_df: pd.DataFrame = pd.concat(df_list, axis=0, ignore_index=True)
         
-        # Generate a date prefix for the filename based on the current date
-        date_prefix: str = datetime.now().strftime('%Y%m%d')  # Format: 'YYYYMMDD'
+        # Count the number of rows before removing duplicates
+        rows_before = combined_df.shape[0]
         
-        # Create the full path for the output file with the date prefix
-        output_file: str = os.path.join(output_dir_abs, f'aggregated_jobs_{date_prefix}.csv')
+        # Drop duplicates based on the 'job_url' column
+        combined_df.drop_duplicates(subset='job_url', inplace=True)
+        
+        # Count the number of rows after removing duplicates
+        rows_after = combined_df.shape[0]
+        
+        # Calculate and print the number of removed duplicates
+        duplicates_removed = rows_before - rows_after
+        print(f"Initial number of data rows: {rows_before}")
+        print(f"Number of duplicates removed: {duplicates_removed}")
+        print(f"Final number of data rows: {rows_after}")
+        
+        # Create the full path for the output file
+        output_file: str = os.path.join(output_dir_abs, 'aggregated_jobs.csv')
         
         # Save the combined DataFrame to the CSV file
         combined_df.to_csv(output_file, index=False)
         print(f"Aggregated data saved to {output_file}")
+        
+        # Delete individual run files after successful aggregation
+        # for f in all_files:
+        #     try:
+        #         os.remove(f)
+        #         print(f"Deleted file: {f}")
+        #     except OSError as e:
+        #         print(f"Error deleting file {f}: {e.strerror}")
+        return output_file
     else:
         print("No data to write.")
 
-# Usage
-source_dir = 'data/individual_scrapers'
-output_dir = '/app/data/aggregated'
-aggregate_csv_files(source_dir, output_dir)
+if __name__ == "__main__":
+    # Directories for the source of individual CSV files and the output for the aggregated file
+    source_dir = 'data/individual_run'
+    output_dir = '/app/data/aggregated'
+    
+    # Call the function to aggregate CSV files
+    aggregate_csv_files(source_dir, output_dir)
