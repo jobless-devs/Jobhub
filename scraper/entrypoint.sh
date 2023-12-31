c2c98e61654eb7c4559ea65d31f69b277fd2a051
sbshaun <sbshaun7@gmail.com>
2023-12-31 00:52:55 -0800

    updated code

--- scraper/entrypoint.sh
+++ scraper/entrypoint.sh
@@ -3,9 +3,9 @@
 echo "Running scraper"
 # this command will run the scraper, but it will not keep the container running
 # for development purposes, we don't need to run the scraper every time we run the container
-# python /opt/jobhub-scraper/main.py
+python /opt/jobhub-scraper/main.py
 
 
 # Then keep the container running
-tail -f /dev/null
-echo "Scraper container is up" 
+# tail -f /dev/null
+echo "Scraper container finsihed running" 
