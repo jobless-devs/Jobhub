be38f88a84035054d476a4713e8dce1c9a57b872
thedarianwong <thedarianwong@gmail.com>
2023-12-31 08:06:44 -0800

    Landing page draft

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -10,44 +10,52 @@ const HomePage = () => {
         <meta name="description" content="Find your dream job with Job Hub" />
       </Head>
 
-      <div className="container mx-auto px-4 h-screen w-full flex flex-col justify-center">
-        <nav className="flex justify-between items-center py-4">
-          <div className="jobhub-logo">
-            {/* Placeholder*/}
-            <span>Logo</span>
-          </div>
-          <div className="flex space-x-4">
-            <a href="#about" className="hover:underline">About Us</a>
-            <a href="#jobs" className="hover:underline">Jobs</a>
-            <a href="#login" className="hover:underline">Log In</a>
-          </div>
-        </nav>
-
-        <div className="flex flex-1 items-center justify-center">
-          {/* Placeholder*/}
-          <div className="w-1/2">
-            {/* Placeholder*/}
-            <div className="wallpaper-image">
-              <span>Placeholder</span>
+      <div className="bg-wallpaper">
+        {/* Landing Page */}
+        <div className="md:container md:mx-auto w-full h-screen flex flex-col justify-center py-4">
+          <nav className="flex justify-between items-center py-4">
+            <div className="jobhub-logo">
+              <img src="/images/Jobhub Logo.png" alt="Job Hub" className="w-60"/> 
             </div>
-          </div>
-
-          <div className="w-1/2 text-right space-y-6">
-            <h1 className="text-6xl font-bold">Your Career Starts Here!</h1>
-            <p className="text-xl">
-              Unleash your potential with real-world experiences. Explore, learn, and grow with our
-              diverse internship opportunities.
-            </p>
-            <div className="space-x-4">
-              <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
-                Find Job
-              </button>
-              <button className="bg-transparent hover:bg-yellow-500 text-yellow-700 font-semibold hover:text-white py-2 px-4 border border-yellow-500 hover:border-transparent rounded">
-                Learn More
-              </button>
+            <div className="flex space-x-16">
+              <a href="#jobs" className="hover:bg-custom-black text-white px-6 py-2 rounded-full ">Jobs</a>
+              <a href="#about" className="hover:bg-custom-black text-white px-6 py-2 rounded-full ">About Us</a>
+              <a href="#login" className="hover:bg-custom-black text-white px-6 py-2 rounded-full border">Log In</a>
+            </div>
+          </nav>
+          <div className="flex flex-1 mt-[18%]"> {/* Using margin to solve for now */}
+            <div className="w-1/2">
+              <div className="wallpaper-image">
+                <img src="/images/Jobhub Landing.png" alt="Job Hub Landing Page" className="w-full px-10"/>
+              </div>
+            </div>
+            <div className="w-1/2 text-center space-y-6 px-12">
+              <h1 className="text-4xl font-bold">Your Career Starts Here! &#9654;</h1>
+              <p className="text-xl text-center">
+                Unleash your potential with real-world experiences. Explore, learn, and grow with our
+                diverse internship opportunities.
+              </p>
+              <div className="flex justify-center items-center space-x-6 ">
+                <button className="bg-custom-black hover:bg-custom-orange text-white font-semibold py-2 px-4 rounded-full">
+                  Find Job
+                </button>
+                <button className="bg-custom-gray hover:bg-custom-orange text-white font-semibold hover:text-white py-2 px-4 rounded-full">
+                  Learn More
+                </button>
+              </div>
             </div>
           </div>
         </div>
+        {/* Dare to Dream Section */}
+        <div className="wallpaper-image"> {/* Using image for now, will use the actual logo and texts*/}
+                <img src="/images/Dream Logo.png" alt="Dream Big Page" className="w-full px-10"/>
+        </div>
+
+        {/* Jobs Section */}
+        
+
+
+
       </div>
     </>
   );
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -8,6 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'custom-orange': '#ffa31a',
+        'custom-gray': '#808080',
+        'custom-dark-gray': '#292929',
+        'custom-black': '#1b1b1b',
+        'custom-white': '#ffffff',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
